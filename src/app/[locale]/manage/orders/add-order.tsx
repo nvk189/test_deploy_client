15f6401a416640c917ec1d7dd0b0198cf8f0a51b
nvk189 <khoa69021@gmail.com>
2025-06-09 08:52:11 +0700

    update buttom create

--- src/app/[locale]/manage/orders/add-order.tsx
+++ src/app/[locale]/manage/orders/add-order.tsx
@@ -1,147 +1,147 @@
-'use client'
+"use client";
 import {
   Dialog,
   DialogContent,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger
-} from '@/components/ui/dialog'
-import { PlusCircle } from 'lucide-react'
-import { useMemo, useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { PlusCircle } from "lucide-react";
+import { useMemo, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   GuestLoginBody,
-  GuestLoginBodyType
-} from '@/schemaValidations/guest.schema'
-import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Button } from '@/components/ui/button'
-import { TablesDialog } from '@/app/[locale]/manage/orders/tables-dialog'
-import { GetListGuestsResType } from '@/schemaValidations/account.schema'
-import { Switch } from '@/components/ui/switch'
-import GuestsDialog from '@/app/[locale]/manage/orders/guests-dialog'
-import { CreateOrdersBodyType } from '@/schemaValidations/order.schema'
-import Quantity from '@/app/[locale]/guest/menu/quantity'
-import Image from 'next/image'
-import { cn, formatCurrency, handleErrorApi } from '@/lib/utils'
-import { DishStatus } from '@/constants/type'
-import { useDishListQuery } from '@/queries/useDish'
-import { useCreateOrderMutation } from '@/queries/useOrder'
-import { useCreateGuestMutation } from '@/queries/useAccount'
-import { toast } from '@/components/ui/use-toast'
+  GuestLoginBodyType,
+} from "@/schemaValidations/guest.schema";
+import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { TablesDialog } from "@/app/[locale]/manage/orders/tables-dialog";
+import { GetListGuestsResType } from "@/schemaValidations/account.schema";
+import { Switch } from "@/components/ui/switch";
+import GuestsDialog from "@/app/[locale]/manage/orders/guests-dialog";
+import { CreateOrdersBodyType } from "@/schemaValidations/order.schema";
+import Quantity from "@/app/[locale]/guest/menu/quantity";
+import Image from "next/image";
+import { cn, formatCurrency, handleErrorApi } from "@/lib/utils";
+import { DishStatus } from "@/constants/type";
+import { useDishListQuery } from "@/queries/useDish";
+import { useCreateOrderMutation } from "@/queries/useOrder";
+import { useCreateGuestMutation } from "@/queries/useAccount";
+import { toast } from "@/components/ui/use-toast";
 
 export default function AddOrder() {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   const [selectedGuest, setSelectedGuest] = useState<
-    GetListGuestsResType['data'][0] | null
-  >(null)
-  const [isNewGuest, setIsNewGuest] = useState(true)
-  const [orders, setOrders] = useState<CreateOrdersBodyType['orders']>([])
-  const { data } = useDishListQuery()
-  const dishes = useMemo(() => data?.payload.data ?? [], [data])
+    GetListGuestsResType["data"][0] | null
+  >(null);
+  const [isNewGuest, setIsNewGuest] = useState(true);
+  const [orders, setOrders] = useState<CreateOrdersBodyType["orders"]>([]);
+  const { data } = useDishListQuery();
+  const dishes = useMemo(() => data?.payload.data ?? [], [data]);
 
   const totalPrice = useMemo(() => {
     return dishes.reduce((result, dish) => {
-      const order = orders.find((order) => order.dishId === dish.id)
-      if (!order) return result
-      return result + order.quantity * dish.price
-    }, 0)
-  }, [dishes, orders])
-  const createOrderMutation = useCreateOrderMutation()
-  const createGuestMutation = useCreateGuestMutation()
+      const order = orders.find((order) => order.dishId === dish.id);
+      if (!order) return result;
+      return result + order.quantity * dish.price;
+    }, 0);
+  }, [dishes, orders]);
+  const createOrderMutation = useCreateOrderMutation();
+  const createGuestMutation = useCreateGuestMutation();
 
   const form = useForm<GuestLoginBodyType>({
     resolver: zodResolver(GuestLoginBody),
     defaultValues: {
-      name: '',
-      tableNumber: 0
-    }
-  })
-  const name = form.watch('name')
-  const tableNumber = form.watch('tableNumber')
+      name: "",
+      tableNumber: 0,
+    },
+  });
+  const name = form.watch("name");
+  const tableNumber = form.watch("tableNumber");
 
   const handleQuantityChange = (dishId: number, quantity: number) => {
     setOrders((prevOrders) => {
       if (quantity === 0) {
-        return prevOrders.filter((order) => order.dishId !== dishId)
+        return prevOrders.filter((order) => order.dishId !== dishId);
       }
-      const index = prevOrders.findIndex((order) => order.dishId === dishId)
+      const index = prevOrders.findIndex((order) => order.dishId === dishId);
       if (index === -1) {
-        return [...prevOrders, { dishId, quantity }]
+        return [...prevOrders, { dishId, quantity }];
       }
-      const newOrders = [...prevOrders]
-      newOrders[index] = { ...newOrders[index], quantity }
-      return newOrders
-    })
-  }
+      const newOrders = [...prevOrders];
+      newOrders[index] = { ...newOrders[index], quantity };
+      return newOrders;
+    });
+  };
 
   const handleOrder = async () => {
     try {
-      let guestId = selectedGuest?.id
+      let guestId = selectedGuest?.id;
       if (isNewGuest) {
         const guestRes = await createGuestMutation.mutateAsync({
           name,
-          tableNumber
-        })
-        guestId = guestRes.payload.data.id
+          tableNumber,
+        });
+        guestId = guestRes.payload.data.id;
       }
       if (!guestId) {
         toast({
-          description: 'Hãy chọn một khách hàng'
-        })
-        return
+          description: "Hãy chọn một khách hàng",
+        });
+        return;
       }
       await createOrderMutation.mutateAsync({
         guestId,
-        orders
-      })
-      reset()
+        orders,
+      });
+      reset();
     } catch (error) {
       handleErrorApi({
         error,
-        setError: form.setError
-      })
+        setError: form.setError,
+      });
     }
-  }
+  };
 
   const reset = () => {
-    form.reset()
-    setSelectedGuest(null)
-    setIsNewGuest(true)
-    setOrders([])
-    setOpen(false)
-  }
+    form.reset();
+    setSelectedGuest(null);
+    setIsNewGuest(true);
+    setOrders([]);
+    setOpen(false);
+  };
 
   return (
     <Dialog
       onOpenChange={(value) => {
         if (!value) {
-          reset()
+          reset();
         }
-        setOpen(value)
+        setOpen(value);
       }}
       open={open}
     >
       <DialogTrigger asChild>
-        <Button size='sm' className='h-7 gap-1'>
+        {/* <Button size='sm' className='h-7 gap-1'>
           <PlusCircle className='h-3.5 w-3.5' />
           <span className='sr-only sm:not-sr-only sm:whitespace-nowrap'>
             Tạo đơn hàng
           </span>
-        </Button>
+        </Button> */}
       </DialogTrigger>
-      <DialogContent className='sm:max-w-[600px] max-h-screen overflow-auto'>
+      <DialogContent className="sm:max-w-[600px] max-h-screen overflow-auto">
         <DialogHeader>
           <DialogTitle>Tạo đơn hàng</DialogTitle>
         </DialogHeader>
-        <div className='grid grid-cols-4 items-center justify-items-start gap-4'>
-          <Label htmlFor='isNewGuest'>Khách hàng mới</Label>
-          <div className='col-span-3 flex items-center'>
+        <div className="grid grid-cols-4 items-center justify-items-start gap-4">
+          <Label htmlFor="isNewGuest">Khách hàng mới</Label>
+          <div className="col-span-3 flex items-center">
             <Switch
-              id='isNewGuest'
+              id="isNewGuest"
               checked={isNewGuest}
               onCheckedChange={setIsNewGuest}
             />
@@ -151,19 +151,19 @@ export default function AddOrder() {
           <Form {...form}>
             <form
               noValidate
-              className='grid auto-rows-max items-start gap-4 md:gap-8'
-              id='add-employee-form'
+              className="grid auto-rows-max items-start gap-4 md:gap-8"
+              id="add-employee-form"
             >
-              <div className='grid gap-4 py-4'>
+              <div className="grid gap-4 py-4">
                 <FormField
                   control={form.control}
-                  name='name'
+                  name="name"
                   render={({ field }) => (
                     <FormItem>
-                      <div className='grid grid-cols-4 items-center justify-items-start gap-4'>
-                        <Label htmlFor='name'>Tên khách hàng</Label>
-                        <div className='col-span-3 w-full space-y-2'>
-                          <Input id='name' className='w-full' {...field} />
+                      <div className="grid grid-cols-4 items-center justify-items-start gap-4">
+                        <Label htmlFor="name">Tên khách hàng</Label>
+                        <div className="col-span-3 w-full space-y-2">
+                          <Input id="name" className="w-full" {...field} />
                           <FormMessage />
                         </div>
                       </div>
@@ -172,17 +172,17 @@ export default function AddOrder() {
                 />
                 <FormField
                   control={form.control}
-                  name='tableNumber'
+                  name="tableNumber"
                   render={({ field }) => (
                     <FormItem>
-                      <div className='grid grid-cols-4 items-center justify-items-start gap-4'>
-                        <Label htmlFor='tableNumber'>Chọn bàn</Label>
-                        <div className='col-span-3 w-full space-y-2'>
-                          <div className='flex items-center gap-4'>
+                      <div className="grid grid-cols-4 items-center justify-items-start gap-4">
+                        <Label htmlFor="tableNumber">Chọn bàn</Label>
+                        <div className="col-span-3 w-full space-y-2">
+                          <div className="flex items-center gap-4">
                             <div>{field.value}</div>
                             <TablesDialog
                               onChoose={(table) => {
-                                field.onChange(table.number)
+                                field.onChange(table.number);
                               }}
                             />
                           </div>
@@ -198,14 +198,14 @@ export default function AddOrder() {
         {!isNewGuest && (
           <GuestsDialog
             onChoose={(guest) => {
-              setSelectedGuest(guest)
+              setSelectedGuest(guest);
             }}
           />
         )}
         {!isNewGuest && selectedGuest && (
-          <div className='grid grid-cols-4 items-center justify-items-start gap-4'>
-            <Label htmlFor='selectedGuest'>Khách đã chọn</Label>
-            <div className='col-span-3 w-full gap-4 flex items-center'>
+          <div className="grid grid-cols-4 items-center justify-items-start gap-4">
+            <Label htmlFor="selectedGuest">Khách đã chọn</Label>
+            <div className="col-span-3 w-full gap-4 flex items-center">
               <div>
                 {selectedGuest.name} (#{selectedGuest.id})
               </div>
@@ -218,13 +218,13 @@ export default function AddOrder() {
           .map((dish) => (
             <div
               key={dish.id}
-              className={cn('flex gap-4', {
-                'pointer-events-none': dish.status === DishStatus.Unavailable
+              className={cn("flex gap-4", {
+                "pointer-events-none": dish.status === DishStatus.Unavailable,
               })}
             >
-              <div className='flex-shrink-0 relative'>
+              <div className="flex-shrink-0 relative">
                 {dish.status === DishStatus.Unavailable && (
-                  <span className='absolute inset-0 flex items-center justify-center text-sm'>
+                  <span className="absolute inset-0 flex items-center justify-center text-sm">
                     Hết hàng
                   </span>
                 )}
@@ -234,17 +234,17 @@ export default function AddOrder() {
                   height={100}
                   width={100}
                   quality={100}
-                  className='object-cover w-[80px] h-[80px] rounded-md'
+                  className="object-cover w-[80px] h-[80px] rounded-md"
                 />
               </div>
-              <div className='space-y-1'>
-                <h3 className='text-sm'>{dish.name}</h3>
-                <p className='text-xs'>{dish.description}</p>
-                <p className='text-xs font-semibold'>
+              <div className="space-y-1">
+                <h3 className="text-sm">{dish.name}</h3>
+                <p className="text-xs">{dish.description}</p>
+                <p className="text-xs font-semibold">
                   {formatCurrency(dish.price)}
                 </p>
               </div>
-              <div className='flex-shrink-0 ml-auto flex justify-center items-center'>
+              <div className="flex-shrink-0 ml-auto flex justify-center items-center">
                 <Quantity
                   onChange={(value) => handleQuantityChange(dish.id, value)}
                   value={
@@ -257,7 +257,7 @@ export default function AddOrder() {
           ))}
         <DialogFooter>
           <Button
-            className='w-full justify-between'
+            className="w-full justify-between"
             onClick={handleOrder}
             disabled={orders.length === 0}
           >
@@ -267,5 +267,5 @@ export default function AddOrder() {
         </DialogFooter>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
